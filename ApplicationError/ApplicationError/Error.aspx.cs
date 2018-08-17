1a8324417f941dd6dd955ae546905f8d82e5d56b
Mauricio Junior <mauricio.junior@compline.com.br>
2018-08-17 19:10:03 -0300

    Projeto Application Error Funcionando

---
+++ ApplicationError/ApplicationError/Error.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApplicationError
+{
+    public partial class Error : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Exception caughtException = (Exception)Application["TheException"];
+                Response.Write(caughtException.InnerException.ToString() + "<br>" + caughtException.Message.ToString() + "<br>" + caughtException.Source.ToString() + "<br>" + caughtException.StackTrace.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
